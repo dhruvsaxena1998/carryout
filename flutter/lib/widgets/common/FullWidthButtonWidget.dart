1d8ee6d9ec9b101b57126b170ae66105f1f34010
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2021-03-29 01:05:33 +0530

    moved app to seperate directory

---
+++ flutter/lib/widgets/common/FullWidthButtonWidget.dart
@@ -0,0 +1,39 @@
+import 'package:carryout/theme.dart';
+import 'package:flutter/material.dart';
+
+class FullWidthButtonWidget extends StatelessWidget {
+  final String title;
+
+  FullWidthButtonWidget({Key key, @required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {},
+      customBorder: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Container(
+          width: MediaQuery.of(context).size.width - 30,
+          decoration: BoxDecoration(color: AppTheme.colors.accent),
+          padding: const EdgeInsets.all(10),
+          child: Align(
+            alignment: Alignment.center,
+            child: Text(
+              title.toUpperCase(),
+              style: TextStyle(
+                color: AppTheme.colors.dark,
+                fontSize: 20,
+                fontFamily: 'Poppins',
+                fontWeight: FontWeight.w700,
+                letterSpacing: 1.2,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
