64e849aebdce4bf2e283e85cc0022cbdf0029dad
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2021-03-31 22:06:48 +0530

    added scripts for deployment

---
+++ scripts/application_install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Downloads node and npm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install v14.16.0 # latest as of 31-03-2021
+
+# Create working directory if it doesn't exists already
+DIR="/home/ec2-user/carryout"
+if [ -d "$DIR" ]; then
+  echo "$DIR exists"
+else
+  echo "Creating $DIR"
+  mkdir "$DIR"
+fi
+
---
+++ scripts/application_start.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Give permissions
+sudo chmod -R 777 /home/ec2-user/carryout/server
+
+# Navigate to working directory
+cd /home/ec2-user/carryout/server
+
+# Add npm and node to PATH
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Loads nvm	
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion" # Loads bash completion (node is available in PATH now)
+
+
+# Install globall packages
+npm i -g yarn@latest pm2 serve
+
+source ~/.bashrc
+
+
+# Install packages
+yarn install
+
+# Start production env. in background
+pm2 start npm --name "server" -- run production
