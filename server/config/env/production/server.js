4739efa1774d3394b9c4c643fc6b3f17d93637a9
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2021-03-31 22:36:31 +0530

    changed server port

--- server/config/env/production/server.js
+++ server/config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 8080),
+  port: env.int('PORT', 1337),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '5050ac1d6a35ae56a2f2604d838ddacc'),
