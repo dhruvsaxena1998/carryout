9426f504c1029c1e05da66adda08793cdcdc0d39
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2021-04-18 12:03:37 +0530

    Added jsdoc @typedefs and enabled Flipper

---
+++ app/src/@types/Menu.js
@@ -0,0 +1,11 @@
+/**
+ * @typedef Menu {
+ *  _id: String,
+ *  name: String,
+ *  description: String,
+ *  price: number,
+ *  __v: number,
+ *  image: @type { import('./MediaImage').MediaImage }
+ * }
+ */
+export const unused = {};
