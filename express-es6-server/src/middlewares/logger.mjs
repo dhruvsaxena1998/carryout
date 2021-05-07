0b187a2ffa131180b232a0e76c38ffe0e46c827f
dhruvsaxena1998 <saxenadhruv1927@gmail.com>
2021-05-08 01:04:16 +0530

    updated logger to save req.body

--- express-es6-server/src/middlewares/logger.mjs
+++ express-es6-server/src/middlewares/logger.mjs
@@ -1,19 +1,19 @@
 import fs from "fs";
-import chalk from "chalk";
 import dayjs from "dayjs";
 
 export default (req, res, next) => {
   const { method, url } = req;
-  const log = `[${dayjs().format(
-    "DD MMM, YY - HH:mm:SSS"
-  )}] - ${method} - ${url}`;
+  const time = dayjs().format("DD MMM, YY - HH:mm:SSS");
+  let log = `[${time}] - ${method} - ${url}`;
+
+  if (method === "POST" && req.body) {
+    log += ` - ${JSON.stringify(req.body)}`;
+  }
 
   fs.appendFile("request_logs.txt", log + "\n", (err) => {
     if (err) {
       console.error(err);
     }
-
-    console.log(chalk.blue("[logger] - ") + log);
   });
   next();
 };
